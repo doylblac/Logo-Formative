ac4e54fd08f98d5ec7b127c656ccb7373a92b741
DoyleandEmily <DoyleandEmily@DOYLE>
2016-09-16 13:01:04 -0400

    Add project files.

---
+++ Logo Formative/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Logo_Formative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.pacmanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.playButton.Location = new System.Drawing.Point(50, 39);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(191, 83);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.playButton.MouseLeave += new System.EventHandler(this.playButton_MouseLeave);
+            this.playButton.MouseHover += new System.EventHandler(this.playButton_MouseHover);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.Black;
+            this.countdownLabel.Location = new System.Drawing.Point(50, 190);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(191, 23);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "Are You My Mom?";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdownLabel.Click += new System.EventHandler(this.countdownLabel_Click);
+            // 
+            // pacmanButton
+            // 
+            this.pacmanButton.Location = new System.Drawing.Point(106, 226);
+            this.pacmanButton.Name = "pacmanButton";
+            this.pacmanButton.Size = new System.Drawing.Size(75, 23);
+            this.pacmanButton.TabIndex = 2;
+            this.pacmanButton.Text = "Run";
+            this.pacmanButton.UseVisualStyleBackColor = true;
+            this.pacmanButton.Click += new System.EventHandler(this.pacmanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pacmanButton);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.Button pacmanButton;
+    }
+}
+
---
+++ Logo Formative/Form1.cs
@@ -0,0 +1,150 @@
+﻿//Created by Doyle Blacklock on September 15, 2016
+//Created to make a Logo for Central Rams Arcade
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace Logo_Formative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            pacmanButton.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer One = new SoundPlayer(Properties.Resources._71548__lindsayanng__1); //Setting up the "one" sound
+            SoundPlayer Two = new SoundPlayer(Properties.Resources._71549__lindsayanng__2); //Setting up the "two Sound
+            SoundPlayer Three = new SoundPlayer(Properties.Resources._71550__lindsayanng__3__1_); //Setting up the "three" sound
+            SoundPlayer Go = new SoundPlayer(Properties.Resources._132685__ashjenx__lets_go); //Setting up the "go" sound
+
+           Graphics formGraphics = this.CreateGraphics();//Allows for graphics to be created
+            SolidBrush WhiteBrush = new SolidBrush(Color.White);//Creates a white brush
+            SolidBrush RedBrush = new SolidBrush(Color.Red);//Creates a red brush
+            Font BigFont = new Font("Times New Roman", 80, FontStyle.Bold);//Creates a New font colour and size
+            Font MediumFont = new Font("Deja Vu Serif", 30, FontStyle.Bold);//Creates a New font colour and size
+            Font LittleFont = new Font("Deja Vu Serif", 7, FontStyle.Bold);//Creates a New font colour and size
+
+            playButton.Visible = false; //Making the button invisible
+            
+/*
+            countdownLabel.ForeColor = Color.White; //Making the label visible
+            countdownLabel.Text = "Game will start in: 3"; //Changing the text of the label
+            Three.Play(); //Playing sound for three
+
+            Refresh(); //Putting the computer to sleep
+            Thread.Sleep(1000);  //Putting the computer to sleep
+
+            countdownLabel.Text = "Game will start in: 2"; //Changing the text of the label
+            Two.Play(); //Playing sound for two
+
+            Refresh();//Putting the computer to sleep
+            Thread.Sleep(1000); //Putting the computer to sleep
+
+            countdownLabel.Text = "Game will start in: 1"; //Changing the text of the label
+            One.Play(); //Playing sound for one
+
+            Refresh(); //Putting the computer to sleep
+            Thread.Sleep(1000);//Putting the computer to sleep */
+
+            countdownLabel.Visible = false;//Makes the label invisible
+
+        /*    BackColor = Color.Green; //Changing the color of the entire design to green
+
+            Refresh();//Refreshs the screen and allows the images to be stacked
+
+            formGraphics.FillEllipse(RedBrush, 65, 90, 160, 110);//Draws the Ellispe
+            formGraphics.DrawString("C", BigFont, WhiteBrush, 70, 85);//Writes the C for the logo
+            formGraphics.DrawString("Rams", MediumFont, WhiteBrush, 110, 120);//Writes Rams for the logo
+
+            formGraphics.TranslateTransform(225, 200);//Moves the rotated text
+            formGraphics.RotateTransform(270);//Rotates the text 270 degrees
+            formGraphics.DrawString("Central Rams Arcade", LittleFont, WhiteBrush, new Rectangle());//Writes the side text
+            formGraphics.ResetTransform();//Resets the rotation of the screen
+            
+           */
+          // Thread.Sleep(2000); 
+
+            BackColor = Color.Black;
+
+            pacmanButton.Visible = true;
+        }
+
+        private void playButton_MouseHover(object sender, EventArgs e)
+        {
+            playButton.BackColor = Color.Green; //Changing the color of the button to black when the button leaves it
+        }
+
+
+
+        private void playButton_MouseLeave(object sender, EventArgs e)
+        {
+            playButton.BackColor = Color.Black;
+
+        }
+
+        private void countdownLabel_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void pacmanButton_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            Pen bluePen = new Pen(Color.Blue, 7);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+            formGraphics.TranslateTransform(175, 7);
+            formGraphics.RotateTransform(45);
+            formGraphics.DrawLine(bluePen, 0, 0, 100, 100);
+            formGraphics.ResetTransform();
+
+            formGraphics.TranslateTransform(178, 10);
+            formGraphics.RotateTransform(135);
+            formGraphics.DrawLine(bluePen, 0, 0, 120, 120);
+            formGraphics.ResetTransform();
+
+            formGraphics.TranslateTransform(100, 80);
+            formGraphics.RotateTransform(135);
+            formGraphics.DrawLine(bluePen, 0, 0, 65, 65);
+            formGraphics.ResetTransform();
+
+            formGraphics.TranslateTransform(100, 77);
+            formGraphics.RotateTransform(45);
+            formGraphics.DrawLine(bluePen, 0, 0, 50, 50);
+            formGraphics.ResetTransform();
+
+           // formGraphics.FillEllipse(yellowBrush, 10, 20, 50, 50);
+
+            formGraphics.FillPie(yellowBrush, 10, 20, 50, 50, 120, 300);
+
+
+        }
+
+
+
+
+
+
+
+    }
+    }
+
+
